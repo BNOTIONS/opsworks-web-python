d287d2d63fdfd87630f5c39a9f25b42322937a6a
Lee Newton <lee@bnotions.com>
2015-03-20 15:50:32 -0400

    naughty quotes

--- opsworks_deploy_python/definitions/django_base.rb
+++ opsworks_deploy_python/definitions/django_base.rb
@@ -62,7 +62,7 @@ define :django_configure do
     
     if gunicorn["enabled"]
       include_recipe 'supervisor'
-      base_command = "#{::File.join(deploy[:deploy_to], 'shared', 'env', 'bin', 'gunicorn')} --pythonpath=#{::File.join(deploy[:deploy_to], 'current', 'deploy[:app_module]')}"
+      base_command = "#{::File.join(deploy[:deploy_to], 'shared', 'env', 'bin', 'gunicorn')} --pythonpath=#{::File.join(deploy[:deploy_to], 'current', deploy[:app_module])}"
       
       gunicorn_cfg = ::File.join(deploy[:deploy_to], 'shared', 'gunicorn_config.py')
       gunicorn_command = "#{base_command} -c #{gunicorn_cfg} wsgi:application"
