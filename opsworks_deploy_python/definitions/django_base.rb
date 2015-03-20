a65c694332530fdebcf997e8999313766720a2d0
Lee Newton <lee@bnotions.com>
2015-03-20 11:46:06 -0400

    fix node call

--- opsworks_deploy_python/definitions/django_base.rb
+++ opsworks_deploy_python/definitions/django_base.rb
@@ -62,7 +62,7 @@ define :django_configure do
     
     if gunicorn["enabled"]
       include_recipe 'supervisor'
-      base_command = "#{::File.join(deploy[:deploy_to], 'shared', 'env', 'bin', 'python')} #{::File.join(deploy[application][:app_module], 'manage.py')} run_gunicorn"
+      base_command = "#{::File.join(deploy[:deploy_to], 'shared', 'env', 'bin', 'python')} #{::File.join(deploy[:app_module], 'manage.py')} run_gunicorn"
       
       gunicorn_cfg = ::File.join(deploy[:deploy_to], 'shared', 'gunicorn_config.py')
       gunicorn_command = "#{base_command} -c #{gunicorn_cfg}"
